b54f30537d1d64242a7fde8c9d1dcc71d33e80eb
Brett Hartley <bh9@sanger.ac.uk>
2017-06-13 13:45:56 +0100

    wait until etcd is available on the first node before starting tests

--- test.sh
+++ test.sh
@@ -2,6 +2,14 @@
 set -ex
 set pipefail
 etcd_hosts=$(openstack server list -c Networks -c Name | gawk "/a$CI_BUILD_ID/"' {match($0,/((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/); ip = substr($0,RSTART,RLENGTH); print ip}')
+first_host=$(echo "$etcd_hosts" | cut -d\  -f1)
+x=1
+while [ $((x)) -gt 0 ];do
+    set +e 
+    wget --timeout 30 http://$firsthost:12379/v2/members 
+    x=$?
+    sleep 5
+done
 for i in ${etcd_hosts}; do
     newtotal=$(wget --timeout 30 http://${i}:12379/v2/members | jq '.[] | length')
     if [ -n "$lasttotal" ]; then
@@ -23,7 +31,6 @@ for i in ${etcd_hosts}; do
         fi
     fi
 done
-first_host=$(echo "$etcd_hosts" | cut -d\  -f1)
 crnodes=$(wget --timeout 30 ${first_host}:8080/_status/nodes | jq '.[]|length')
 if [ $((crnodes)) -eq $((lasttotal)) ]; then
     echo cockroach agrees on cluster size with etcd
