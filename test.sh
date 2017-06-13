7754e81a9fc7f958a94abc9c9c95f963b273c8c0
Brett Hartley <bh9@sanger.ac.uk>
2017-06-13 13:05:19 +0100

    pipefail and use wget as the runners don't have curl (oops)

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
+set pipefail
 etcd_hosts=$(openstack server list -c Networks -c Name | gawk "/a$CI_BUILD_ID/"' {match($0,/((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/); ip = substr($0,RSTART,RLENGTH); print ip}')
 for i in etcd_hosts; do
-    newtotal=$(curl -m 30 http://${i}:12379/v2/members | jq '.[] | length')
+    newtotal=$(wget --timeout 30 http://${i}:12379/v2/members | jq '.[] | length')
     if [ -n "$lasttotal" ]; then
         if [ $((newtotal)) -eq $((lasttotal)) ]; then
             echo "$i" agrees
@@ -23,7 +24,7 @@ for i in etcd_hosts; do
     fi
 done
 first_host=$(echo "$etcd_hosts" | cut -d\  -f1)
-crnodes=$(curl ${first_host}:8080/_status/nodes | jq '.[]|length')
+crnodes=$(wget --timeout 30 ${first_host}:8080/_status/nodes | jq '.[]|length')
 if [ $((crnodes)) -eq $((lasttotal)) ]; then
     echo cockroach agrees on cluster size with etcd
     exit 0
