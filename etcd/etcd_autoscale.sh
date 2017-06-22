46655a7c43e12092e4a8038d549f46449641f279
Brett Hartley <bh9@sanger.ac.uk>
2017-06-22 11:44:27 +0100

    forgot a $(

--- etcd/etcd_autoscale.sh
+++ etcd/etcd_autoscale.sh
@@ -440,7 +440,7 @@ echo $ID
 sleep 5
 openstack server delete --os-region $AWS_DEFAULT_REGION --os-username $OS_USERNAME --os-password $OS_PASSWORD --os-tenant-name $OS_TENANT_NAME --os-auth-url $OS_AUTH_URL $ID #delete yourself
 EOF
-SCALE_URL=openstack stack show -c outputs -f json $asg_name | jq '.outputs | select(.[].output_key=="scale_up_url") | .[0].output_value'
+SCALE_URL=$(openstack stack show -c outputs -f json $asg_name | jq '.outputs | select(.[].output_key=="scale_up_url") | .[0].output_value')
 cat > /var/lib/etcd/recover.sh <<-EOF
 #!/bin/bash
 set -e
